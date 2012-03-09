b0bced9a3b41b58b4faa3b3438da810871f2056c
josegrad <josegrad@gmail.com>
2012-03-09 23:01:43 +0200

    Update lib/core_ext/date.rb

--- lib/core_ext/date.rb
+++ lib/core_ext/date.rb
@@ -14,8 +14,11 @@ class Date
   private
   def feed_utils_to_time(dest, method)
     #Convert a fraction of a day to a number of microseconds
-    usec = (dest.sec_fraction * 60 * 60 * 24 * (10**6)).to_i
+    #usec = (dest.sec_fraction * 60 * 60 * 24 * (10**6)).to_i
     Time.send(method, dest.year, dest.month, dest.day, dest.hour, dest.min,
               dest.sec, usec)
   end
 end
+
+
+
